4cbc5235ec5dddf88ee52e7317059bfa8665e1e9
SantiHerrera18 <santi.herrera318@gmail.com>
2024-11-15 01:10:52 -0500

    Styles reviewed

--- front/src/helpers/searchBar.ts
+++ front/src/helpers/searchBar.ts
@@ -1,25 +1,22 @@
-import axios from 'axios'
+import axios from "axios";
 
 const getResult = async (query: string) => {
+  try {
+    const response = await axios.post(
+      `http://localhost:3000/search/bar?query=${query}`
+    );
 
-try {
-
-    const response = await axios.post(`http://localhost:3000/search/bar?query=${query}`)
-    
     if (response) {
-      console.log(response)
-        const data = await response.data;
-        return Array.isArray(data) ? data : []; 
-      } else {
-        console.error("Error en la respuesta del servidor");
-        return [];
-      }
-    } catch (error) {
-      console.error("Error en la solicitud:", error);
+      console.log(response);
+      const data = await response.data;
+      return Array.isArray(data) ? data : [];
+    } else {
+      console.error("Error en la respuesta del servidor");
       return [];
     }
-
-
-
-}
-export default getResult
+  } catch (error) {
+    console.error("Error en la solicitud:", error);
+    return [];
+  }
+};
+export default getResult;
