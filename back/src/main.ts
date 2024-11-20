b7f0f2f86cdd07bc857371e0aee2a2503f3997cc
Monterolautaro <167786236+Monterolautaro@users.noreply.github.com>
2024-11-19 22:31:47 -0300

    p

--- back/src/main.ts
+++ back/src/main.ts
@@ -11,7 +11,7 @@ config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://proyecto-hoteleria-pjut38ksy-monterolautaros-projects.vercel.app',
+    origin: 'https://proyecto-hoteleria-m3sgbl0uy-monterolautaros-projects.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
