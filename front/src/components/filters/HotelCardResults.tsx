dff81a0bc8eeb9fe64ecca03929cc17468f54bda
soniams0 <sony16mun@gmail.com>
2024-11-19 22:53:25 -0500

    logica de filtrado

--- front/src/components/filters/HotelCardResults.tsx
+++ front/src/components/filters/HotelCardResults.tsx
@@ -1,36 +1,40 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-
+import { Hotel } from "@/interfaces/hotel"; 
 import { useRouter } from "next/navigation";
 
-const HotelCardResults = () => {
+interface HotelCardResultsProps {
+  hotel: Hotel;
+}
+
+const HotelCardResults: React.FC<HotelCardResultsProps> = ({ hotel }) => {
   const router = useRouter();
 
   const handleClick = () => {
-    const id = "12e399cc-d608-42d3-85ce-6120bef3d07d";
-    router.push(`/hotel-detail/${id}`);
+    router.push(`/hotel-detail/${hotel.id}`);
   };
+
   return (
-    <div className="flex p-4 bg-white shadow-md rounded-lg border border-gray-300 mb-4">
+    <div className="flex p-4 bg-gray-50 shadow-md rounded-lg border border-gray-300 mb-4">
       <img
-        src="https://via.placeholder.com/150"
-        alt="Hotel"
+        src={hotel.imageUrl}
+        alt={hotel.title}
         className="w-24 h-24 rounded-md object-cover"
       />
       <div className="ml-4 flex-grow">
-        <h3 className="text-lg font-bold">Hotel Marriot Bogota</h3>
-        <p className="text-sm text-gray-500">Bogota, Colombia</p>
+        <h3 className="text-lg font-bold">{hotel.title}</h3>
+        <p className="text-sm text-gray-500">{hotel.location}</p>
         <p className="text-sm text-teal-600 mt-2">
-          4.8 ★ Excellent (999 reviews)
+          {hotel.rating} ★ Excellent
         </p>
       </div>
       <div className="text-right">
-        <p className="text-lg font-semibold text-teal-600">400000 COP</p>
+        <p className="text-lg font-semibold text-teal-600">{hotel.price} USD</p>
         <button
           onClick={handleClick}
           className="mt-2 px-4 py-2 bg-[#009375] text-white rounded-lg"
         >
-          See all prices
+          See more
         </button>
       </div>
     </div>
