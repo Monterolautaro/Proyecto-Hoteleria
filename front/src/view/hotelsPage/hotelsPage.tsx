e9c59f188664e453f2f4d5e4e566f37f34cabac3
soniams0 <sony16mun@gmail.com>
2024-11-23 13:48:23 -0500

    se modificaron los filtros y la searchbar

--- front/src/view/hotelsPage/hotelsPage.tsx
+++ front/src/view/hotelsPage/hotelsPage.tsx
@@ -1,12 +1,21 @@
-import Filters from '@/components/filters/filters';
-import SearchResults from '@/components/searchBar/SearchResults';
-import React from 'react';
+'use client'
+import Filters from "@/components/filters/filters";
+import SearchResults from "@/components/searchBar/SearchResults";
+import { useState } from "react";
+import { Hotel } from "@/interfaces/hotel";
 
 const HotelsPage = () => {
+  const [hotels, setHotels] = useState<Hotel[]>([]); 
+
   return (
-    <div>
-      <Filters />
-      <SearchResults/>
+    <div className="grid grid-cols-4 gap-6 p-6">
+      <div className="col-span-1 bg-gray-100 p-4 rounded-lg shadow-md">
+        <Filters onFiltersApplied={(results) => setHotels(results)} />
+      </div>
+
+      <div className="col-span-3">
+        <SearchResults hotels={hotels} />
+      </div>
     </div>
   );
 };
