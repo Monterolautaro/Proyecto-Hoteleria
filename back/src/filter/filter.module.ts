456097aff3a801b4440474abebd562d1d42d906e
Thiago <venturathiago0911@gmail.com>
2024-11-22 23:52:46 -0300

    Filtros

--- back/src/filter/filter.module.ts
+++ back/src/filter/filter.module.ts
@@ -6,7 +6,7 @@ import { FilterController } from './filter.controller';
 import { FilterService } from './filter.service';
 import { FilterRepository } from './filter.repository';
 import { Amenities } from 'src/entities/hotel/hotel.amenities.entity';
-import { RoomType } from 'src/entities/hotel/rooms/roomsType.entity';
+import { RoomType } from 'src/entities/hotel/roomsType.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Address, Amenities, RoomType])],
