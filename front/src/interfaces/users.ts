9fc0b0337d501164f66805a8a294510f03520940
SantiHerrera18 <santi.herrera318@gmail.com>
2024-11-27 01:52:19 -0500

    Creación de dashboard y persistencia de sesión

--- front/src/interfaces/users.ts
+++ front/src/interfaces/users.ts
@@ -1,11 +1,22 @@
 export interface User {
-    id: string;
-    name: string;
-    lastname: string;
-    email: string;
-    username: string
-    phone: string;
-    role: string;
-    bookings: number;
-  }
+  id: string;
+  name: string;
+  lastname: string;
+  email: string;
+  username: string;
+  phone: string;
+  role: string;
+  bookings: number;
+}
 
+export interface IUserData {
+  name: string;
+  lastname: string;
+  birthdate: Date;
+}
+
+export interface IUserCredentials {
+  email: string;
+  username: string;
+  password?: string;
+}
