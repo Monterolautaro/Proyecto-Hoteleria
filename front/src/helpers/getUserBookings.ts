59a030a2a5a0ca804666e8a648dbf6536f7e599c
soniams0 <sony16mun@gmail.com>
2024-12-02 16:29:32 -0500

    almacenamiento en cookies de auth de google

--- front/src/helpers/getUserBookings.ts
+++ front/src/helpers/getUserBookings.ts
@@ -1,11 +1,21 @@
 import { UserBookings } from "@/interfaces/bookings";
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const getBookingsByUserId = async (id: string): Promise<UserBookings[] | undefined> => {
   try {
-    const response = await axios.get(`${API_URL}/users/bookings`);
+    const token = Cookies.get("token");
+    if (!token) {
+      throw new Error("No token found");
+    }
+
+    const response = await axios.get(`${API_URL}/users/bookings/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     return response?.data;
   } catch (error) {
     console.error("Error fetching bookings by user ID:", error);
