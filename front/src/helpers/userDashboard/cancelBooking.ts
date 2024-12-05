8846084f705d877647e2410f4fad51d6aecf1e1a
Monterolautaro <167786236+Monterolautaro@users.noreply.github.com>
2024-12-05 01:50:16 -0300

    feat: endpoint de delete booking conectado con el front

--- front/src/helpers/userDashboard/cancelBooking.ts
+++ front/src/helpers/userDashboard/cancelBooking.ts
@@ -3,9 +3,11 @@ import axios from "axios";
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const cancelBooking = async (bookId: string, token: string) => {
+  console.log(' este es el token de google :', token);
+  
   try {
     const response = await axios.put(
-      `${API_URL}/bookings/soft-delete/${bookId}`,
+      `${API_URL}/bookings/soft-delete/${bookId}`, { note: "Cancelado" },
       {
         headers: {
           Authorization: `Bearer ${token}`,
